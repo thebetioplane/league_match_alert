6012375f20c0ddb078826a53c96ab37b9e5fb550
thebetioplane <thebetioplane@users.noreply.github.com>
2024-03-03 12:23:51 -0500

    Updated helper scripts to work with riot id (vs summoner names).

--- get_riotid.sh
+++ get_riotid.sh
@@ -10,11 +10,11 @@ fi
 
 if [ -z "$1" ]
 then
-	echo 'Please supply riot id in the form of name/tag as an argument'
+	echo 'Please supply puuid an argument'
 	exit 1
 fi
 
-URL='https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/'$1
+URL='https://americas.api.riotgames.com/riot/account/v1/accounts/by-puuid/'$1
 
 curl -H "X-Riot-Token: $TOKEN" -X GET $URL
 
